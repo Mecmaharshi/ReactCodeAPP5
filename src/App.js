ab2982beeb0c8195e2b62749224e3d12cc10d2a7
Maharshi Ishu Jasraj <mecmaharshi9@gmail.com>
2023-03-27 16:50:37 +0530

    Code for api1 & api2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import FormPage from "./components/form/Form";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/form" element={<FormPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
