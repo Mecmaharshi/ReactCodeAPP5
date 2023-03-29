780adeabc0c4fb85c755f39e81531e586cb0cacc
Maharshi Ishu Jasraj <89892168+Mecmaharshi@users.noreply.github.com>
2023-03-29 13:18:36 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const check_avalability = "https://appmodteam5.azurewebsites.net/api/Check_avalability?"
 
-export const book_room = 'http://20.96.177.162:3000/Api?'
+export const book_room = 'http://20.96.177.162:3678/api/get?'
